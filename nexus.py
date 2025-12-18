51dce5b5f441cb435efe7e70f722ef4d49a8558f
Denperidge <cat@denperidge.com>
2025-12-18 17:22:42 +0100

    Find vortex installer

--- nexus.py
+++ nexus.py
@@ -1,12 +1,30 @@
-import os
+from os import path
 from pathlib import Path
 
 # --- GENERAL TOOLING ---
 DEFAULT_STEAM_COMPATDATA_PATH = Path(Path.home(), ".steam/steam/steamapps/compatdata/")
 
-def install_vortex_to_prefix(prefix):
-    pass
+def vortex_installer_find():
+    current_dir = Path(path.dirname(path.realpath(__file__)))  # Based on the clever code at https://stackoverflow.com/a/5137509
+    vortex_installer = list(Path(current_dir).glob("Vortex*.exe"))
 
+    if len(vortex_installer) == 0:
+        raise FileNotFoundError(
+            "Could not find Vortex mod manager exe!" +
+            "\nDue to Nexus Mods limitations, you have to get it yourself. Sorry boss" +
+            "\n1. Download it from: https://www.nexusmods.com/site/mods/1?tab=files&file_id=5911" +
+            f"\n2. And put it in the same directory as this script: {current_dir}")
+    elif len(vortex_installer) > 1:
+        print("Found multiple vortex exe's. Please select one!")
+        vortex_installer = select(vortex_installer, "Select your preferred vortex installer")
+    else:
+        vortex_installer = vortex_installer[0]
+    return vortex_installer    
+
+
+def install_vortex_to_prefix(prefix):
+    vortex_installer = vortex_installer_find()
+    print(vortex_installer)
 
 # --- FALLOUT NEW VEGAS ---
 
@@ -55,7 +73,7 @@ def select(options, input_title, option_title_func=None):
     if type(options) == dict:
         return options[list(options.keys())[selection-1]]
     else:
-        return options[i-1]
+        return options[selection-1]
 
 
 
@@ -71,5 +89,13 @@ if __name__ == "__main__":
     mode = select_mode()
 
     prefix = mode["find"]()
-    print(prefix)
+
+    actions = [
+        "Install Vortex Mod Manager",
+    ]
+
+    action = select(actions, "Select action")
+    if action == actions[0]:
+        install_vortex_to_prefix(prefix)
+
 
